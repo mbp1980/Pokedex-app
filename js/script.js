50215fb7f69d17f4c6c854b17827c3041c4b91a8
mbp1980 <73567442+mbp1980@users.noreply.github.com>
2020-11-30 20:44:54 -0700

    added pokemon array
    disabled alert and favorite food

--- js/script.js
+++ js/script.js
@@ -1,4 +1,27 @@
-alert('Hello world');
+/*alert('Hello world');
 
 let favoriteFood  = 'Fried Chicken';
-document.write(favoriteFood );
+document.write(favoriteFood );*/
+
+let pokemonList = [
+  {
+    name: 'Bulbasaur',
+    height: .7,
+    type: ['grass', 'poison']
+  }
+  {
+    name: 'Weedle',
+    height: .3,
+    type: ['bug', 'poison']
+  }
+  {
+    name: 'Pidgeot',
+    height: 1.5,
+    type: ['flying', 'normal']
+  }
+  {
+    name: 'Squirtle',
+    height: .5,
+    type: ['water']
+  }
+];
