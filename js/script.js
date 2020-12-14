d60a129037f06567ee6b7b503af4c17b6be1c2c2
mbp1980 <73567442+mbp1980@users.noreply.github.com>
2020-12-13 17:40:05 -0700

    Created forEach loop and IIFE function Lesson 1.5

--- js/script.js
+++ js/script.js
@@ -1,41 +1,45 @@
-/*alert('Hello world');
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      name: 'Bulbasaur',
+      height: .7,
+      type: ['grass', 'poison']
+    },
+    {
+      name: 'Weedle',
+      height: .3,
+      type: ['bug', 'poison']
+    },
+    {
+      name: 'Pidgeot',
+      height: 1.5,
+      type: ['flying', 'normal']
+    },
+    {
+      name: 'Squirtle',
+      height: .5,
+      type: ['water']
+    }
+  ];
+  pokemonList.forEach(function(pokemon) {
+    document.write(pokemon.name + ' is ' + pokemon.height + ' tall ' + '<br />');
+  });
 
-let favoriteFood  = 'Fried Chicken';
-document.write(favoriteFood );*/
-
-let pokemonList = [
-  {
-    name: 'Bulbasaur',
-    height: .7,
-    type: ['grass', 'poison']
-  },
-  {
-    name: 'Weedle',
-    height: .3,
-    type: ['bug', 'poison']
-  },
-  {
-    name: 'Pidgeot',
-    height: 1.5,
-    type: ['flying', 'normal']
-  },
-  {
-    name: 'Squirtle',
-    height: .5,
-    type: ['water']
+  function myLoopFunction(pokemon) {
+    console.log(pokemon.name +  ' is ' + pokemon.height + 'tall');
   }
-];
+  pokemonList.forEach(myLoopFunction);
 
-for (let i=0; i < pokemonList.length; i++){
-  if (pokemonList[i].height <1 && pokemonList[i].height >.5){
-    document.write(pokemonList[i].name + "  (Height .7) <br /> ");
-  }else if (pokemonList[i].height <.5){
-    document.write(pokemonList[i].name + " (Height .5) <br /> ");
-  }else if (pokemonList[i].height <.5){
-    document.write(pokemonList[i].name + " (Height 1.5) <br /> ");
-  }else if (pokemonList[i].height <.6){
-      document.write(pokemonList[i].name + " (Height .5) <br /> ");
-  }else {
-    document.write(pokemonList[i].name + " (Height 1.5) - Wow, that's a big pokemon <br /> ");
+  function getAll() {
+    return pokemonList;
   }
-}
+
+  function add (pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  return {
+    getAll: getAll,
+    add: add
+  };
+  })();
